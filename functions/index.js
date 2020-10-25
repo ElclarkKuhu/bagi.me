0e518782c4277d2ff807a189f78d13a72a603637
ElclarkKuhu <elclarkkuhu@gmail.com>
2020-10-26 02:26:16 +0800

    designing setup page

--- functions/index.js
+++ functions/index.js
@@ -7,3 +7,14 @@ const functions = require('firebase-functions');
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+// Saves a message to the Firebase Realtime Database but sanitizes the text by removing swearwords.
+exports.addMessage = functions.https.onCall((data, context) => {
+    // Message text passed from the client.
+    const text = data.url;
+    // Authentication / user information is automatically added to the request.
+    const uid = context.auth.uid;
+    const name = context.auth.token.name || null;
+    const picture = context.auth.token.picture || null;
+    const email = context.auth.token.email || null;
+});
--- public/src/js/setup.js
+++ public/src/js/setup.js
@@ -1,4 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById("urlname").innerHTML = window.location.origin + '/#'
+
     firebase.auth().useDeviceLanguage();
 
     firebase.auth().onAuthStateChanged(function(user) {
@@ -11,33 +13,9 @@ document.addEventListener('DOMContentLoaded', function() {
             var isAnonymous = user.isAnonymous;
             var uid = user.uid;
             var providerData = user.providerData;
-
-            if (checkData(uid)) {
-                updateData(photoURL)
-            } else {
-                window.location.replace("/setup")
-            }
         } else {
             // user not singed in
             window.location.replace("/login")
         }
     });
 });
-
-async function checkData(uid) {
-    var re = false;
-    firebase.database().ref('data/' + uid).once('value', snapshot => {
-        if (snapshot.hasChild('url')) {
-            firebase.database().ref('uids').once('value' , snapshot2 => {
-                if (snapshot2.hasChild(snapshot.val().url)) {
-                    re = true
-                }
-            })
-        }
-    });
-    return re
-}
-
-function updateData() {
-
-}
