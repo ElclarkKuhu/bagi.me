b045301cac2241d5c69f7c3891c854a709ff5c80
Elclark Kuhu <elclarkkuhu@gmail.com>
2020-12-11 17:26:52 +0800

    improvement to elclark donation page

---
+++ public/elclark/index.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var methods = {
+        "name" : ["Paypal", "DANA", "OVO", "GO-PAY", "LinkAja"],
+        "link" : [
+            "http://paypal.me/elclarkkuhu",
+            "https://saweria.co/elclark",
+            "https://saweria.co/elclark",
+            "https://saweria.co/elclark",
+            "https://saweria.co/elclark"
+        ]
+    }
+
+    showMethods(methods)
+});
+
+function showMethods(methods) {
+    const p1 = "<a href='";
+    const p2 = "'><button>";
+    const p3 = "</button></a>";
+
+    for (i in methods.name) {
+        document.getElementById('methods').innerHTML = document.getElementById('methods').innerHTML + p1 + methods.link[i] + p2 + methods.name[i] + p3;
+        console.log(p1 + methods.link[i] + p2 + methods.name[i] + p3)
+    }
+}
